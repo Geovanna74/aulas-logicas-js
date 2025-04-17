8c7e472cd6ea4af273f07b4a57ffd83280deac82
GEOVANNA CAROLINE CAMPOS LIMA <geovanna_c_lima@estudante.sesisenai.org.br>
2025-04-16 22:18:32 -0300

    inicio da aula de js

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+// declação de variável com var
+
+/*var nome ="Geovanna"
+
+var nome = "Caroline"*/
+
+// declaração de variável com let
+
+// let nome = "Geovanna"
+
+// nome = "Caroline"
+
+const nome ="Geovanna"
+
+nome = "Caroline"
+
+console.log(nome)
